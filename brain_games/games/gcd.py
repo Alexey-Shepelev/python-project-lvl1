ade3fb6fbd7979d9deb03e81f678816e5972012a
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-25 22:53:59 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -14,14 +14,15 @@ def get_question_and_solution():
 
     question = str(number_1) + ' ' + str(number_2)
 
-    if number_1 < number_2:
-        number_1, number_2 = number_2, number_1
+    def find_gcd(a, b):
+        if a < b:
+            a, b = b, a
+        div_remainder = a % b
+        while div_remainder != 0:
+            a = b
+            b = div_remainder
+            div_remainder = a % b
+        return b
 
-    div_remainder = number_1 % number_2
-    while div_remainder != 0:
-        number_1 = number_2
-        number_2 = div_remainder
-        div_remainder = number_1 % number_2
-
-    correct_answer = str(number_2)
+    correct_answer = str(find_gcd(number_1, number_2))
     return(question, correct_answer)
