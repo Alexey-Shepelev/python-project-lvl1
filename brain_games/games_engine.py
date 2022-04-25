9089ec1ed9e96b6d0020e6f499695c14aa982afe
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-25 21:30:08 +0300

    Update games_engine.py

--- brain_games/games_engine.py
+++ brain_games/games_engine.py
@@ -8,19 +8,18 @@ def play_game(game):
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     print(game.game_rule)
-    game_over_msg = (f'Congratulations, {user_name}!')
+    end_game_msg = (f'Congratulations, {user_name}!')
     i = 0
-    while i < 3:
+    for i in range(3):
         question, correct_answer = game.get_question_and_solution()
         print(f'Question: {question}')
         user_answer = input('Your answer: ')
         if user_answer == correct_answer:
             print('Correct!')
-            i += 1
         else:
-            i = 3
-            game_over_msg = (
+            end_game_msg = (
                 f'"{user_answer}" is wrong anwer ;(. Correct answer was'
                 f' "{correct_answer}".\nLet\'s try again, {user_name}!'
             )
-    print(game_over_msg)
+            break
+    print(end_game_msg)
