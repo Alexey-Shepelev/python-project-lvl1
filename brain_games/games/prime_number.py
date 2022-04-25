d7c56f9657c215070bcb97e38ff8ea7eaff01b6c
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-25 23:08:13 +0300

    Update prime_number.py

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -9,19 +9,19 @@ GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_question_and_solution():
     question = randint(1, 1000)
 
-    if question < 2:
-        correct_answer = 'no'
+    def is_prime(x):
+        if x < 2:
+            return('no')
+        elif x == 2:
+            return('yes')
+        i = 2
+        while i <= x ** (1 / 2):
+            if x % i == 0:
+                return('no')
+            else:
+                i += 1
+            return('yes')
 
-    elif question == 2:
-        correct_answer = 'yes'
-
-    i = 2
-    while i <= question ** (1 / 2):
-        if question % i == 0:
-            correct_answer = 'no'
-            break
-        else:
-            i += 1
-            correct_answer = 'yes'
+    correct_answer = is_prime(question)
 
     return (question, correct_answer)
