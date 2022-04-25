01a5865c47ca8fff11ab8ea36d272cb43d986d23
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-25 22:25:13 +0300

    Update calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -9,12 +9,14 @@ GAME_RULE = 'What is the result of the expression?'
 def get_question_and_solution():
     member_1 = randint(1, 20)
     member_2 = randint(1, 20)
-    operators = [
-        ('+', member_1 + member_2),
-        ('-', member_1 - member_2),
-        ('*', member_1 * member_2)
-    ]
-    operator, expression = choice(operators)
+    operators = ('+', '-', '*')
+    operator = choice(operators)
+    if operator == '+':
+        expression = member_1 + member_2
+    elif operator == '-':
+        expression = member_1 - member_2
+    else:
+        expression = member_1 * member_2
 
     question = str(member_1) + ' ' + operator + ' ' + str(member_2)
     correct_answer = str(expression)
