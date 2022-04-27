656fe74664f53f36c88047fe7aa5bfa110d428fe
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-27 15:42:48 +0300

    Update prime_number.py

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -6,22 +6,22 @@ from random import randint
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(x):
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    i = 2
+    while i <= x ** (1 / 2):
+        if x % i == 0:
+            return False
+        else:
+            i += 1
+    return True
+
+
 def get_question_and_solution():
     question = randint(1, 1000)
-
-    def is_prime(x):
-        if x < 2:
-            return('no')
-        elif x == 2:
-            return('yes')
-        i = 2
-        while i <= x ** (1 / 2):
-            if x % i == 0:
-                return('no')
-            else:
-                i += 1
-        return('yes')
-
-    correct_answer = is_prime(question)
+    correct_answer = 'yes' if is_prime(question) else 'no'
 
     return (question, correct_answer)
