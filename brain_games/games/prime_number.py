888e5d3201ebccdc86ef3245e80f7f113d583bf1
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-04-28 23:58:29 +0300

    Update prime_number.py

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -9,14 +9,11 @@ GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(x):
     if x < 2:
         return False
-    elif x == 2:
-        return True
     i = 2
     while i <= x ** (1 / 2):
         if x % i == 0:
             return False
-        else:
-            i += 1
+        i += 1
     return True
 
 
